d8c117a97a73b11656f9e933706361da187c6f33
Angie <angie.godinezv@gmail.com>
2021-07-07 14:33:16 -0700

    Added calibration

--- analysis/graspingDataFrame.py
+++ analysis/graspingDataFrame.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 from scipy.stats import gmean
+from general_functions import makeFlatList
 
 # Takes in the dataframe csv file (reachingData.csv) created from buildReachingDataFrame.py and finds the distance between
 # the thumb and grasping finger, which will ultimately give us the grasping vector.
@@ -34,8 +35,6 @@ def calculate_grip_geometric_mean(input_data):
 
     return subject_gmean_grip
 
-#test1 = calculate_grip_geometric_mean(grip_cal_data[grip_cal_data.subject == 'ag'])
-
 def makeGraspingTrace():
     subject_vals = []
     condition_vals = []
@@ -58,9 +57,9 @@ def makeGraspingTrace():
                 else:
 
                     # Step 1. Find distance between thumb and grasping finger in 3D
-                    distance = np.sqrt((sub_data.grasping_finger_x - sub_data.thumb_x)**2 + (
-                        sub_data.grasping_finger_y - sub_data.thumb_y)**2 + (
-                        sub_data.grasping_finger_x - sub_data.thumb_z)**2) - closed_grip_distance
+                    distance = np.sqrt((sub_data.grasping_finger_x - sub_data.thumb_x) ** 2 + (
+                            sub_data.grasping_finger_y - sub_data.thumb_y) ** 2 + (
+                                               sub_data.grasping_finger_z - sub_data.thumb_z) ** 2) - closed_grip_distance
 
                     time = sub_data.time_stamp - sub_data.time_stamp.iloc[0]
 
@@ -91,6 +90,4 @@ dataFrame = pd.DataFrame(frames)
 dataFrame['thumb_finger_distance'] = dataFrame['thumb_finger_distance'].astype(str).astype(float)
 dataFrame['grip_time'] = dataFrame['grip_time'].astype(str).astype(float)
 
-print(dataFrame.dtypes)
-
-dataFrame.to_csv(r'C:/Users/angie/Git Root/stereo-prehension/data/graspingData.csv', index=False)
+dataFrame.to_csv(r'C:/Users/angie/Box/Projects/2.Stereo-motor relationship/data/graspingData.csv', index=False)
